5a1fcfd3b943f975e56303353f74f90b0deff673
Yanik <yanik@cronos.ru>
2019-03-27 15:00:46 +0300

    Коннектор в кронос

---
+++ test/index.js
@@ -0,0 +1,32 @@
+const test_suite = require('abstract-winston-transport');
+const Cronos = require('../lib/winston-cronos');
+const { createLogger, format, transports } = require('winston');
+const assert = require('assert');
+describe('winston-cronos', function () {
+    const transport = new Cronos({ host: 'lps', port: 3400 });
+    
+    const log = createLogger({
+        format: format.combine(
+            format.splat(),
+            format.simple()
+        ),
+        levels: {
+            error: 3,
+            warn: 4,
+            info: 6,
+            debug: 7
+        },
+        transports: [
+            transport
+        ]
+    });
+
+    it('winston-cronos-send', function () {
+        log.info('test send')
+        // it('should be closeable', function () {
+        //     transport.close();
+        // });
+    });
+});
+
+// test_suite({ name: 'Cronos', Transport: Cronos, construct: { host: 'lps', port: 3400 }})
